c7336a1c1e0f91db67824914cdbf54d8e7d7a687
Abhinav Thakur <abhi2k2thakur@gmail.com>
2023-07-08 16:19:23 +0530

    a
    a

---
+++ Assets/ScriptsAbhinav/Score.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public static Score instance;
+    public TextMeshProUGUI scoreText;
+
+    int score = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        scoreText.text = score.ToString() + "POINTS";
+    }
+
+    public void AddPoint()
+    {
+        score += 10;
+        scoreText.text = score.ToString() + "POINTS";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
